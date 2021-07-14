3f91519635caf8480b123b36e607169099c3f92e
renaldi99 <renaldisimamora67@gmail.com>
2021-07-14 17:42:05 +0700

    complete landing-page

---
+++ frontend/scripts/main.js
@@ -0,0 +1,23 @@
+// slider-testimonial
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 4000,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    767: {
+      items: 1,
+    },
+    992: {
+      items: 1,
+    },
+    1200: {
+      items: 1,
+    },
+    1500: {
+      items: 1,
+    },
+  },
+});
